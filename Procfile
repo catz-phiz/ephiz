6c7610e9201ea549069aa30183b65dff1e19a681
Le Phuc Hieu <leodavs2104@gmail.com>
2023-06-26 14:30:19 +0700

    more2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ephiz.wsgi --log-file -
--- ephiz/settings.py
+++ ephiz/settings.py
@@ -15,12 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8aw8gl^p2+4x*imb#ex6y=-l#f#vm8(gda$g2+*n%jfsu)(mci'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
