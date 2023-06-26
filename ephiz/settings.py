753daf44e3899a5f46003795c243309efa0cf84d
Le Phuc Hieu <leodavs2104@gmail.com>
2023-06-26 15:51:17 +0700

    ....

--- ephiz/settings.py
+++ ephiz/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
